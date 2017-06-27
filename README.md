89b834623ce075f4ea55242dbe685ab347b3bf9c
Marco Casula <mc@clickbit.net>
2017-06-27 16:04:34 -0500

    init commit
    Signed-off-by: Marco Casula <mc@clickbit.net>

---
+++ README.md
@@ -0,0 +1,16 @@
+engine-roulette
+===============
+
+Currently only an intent of create a static and functional oriented library able to calculate (American) Roulette.
+
+List of references and online materials:
+ * Bet list and table layout [www.ildado.com](http://www.ildado.com/roulette_table_layout.html)
+
+## Contribution
+
+Contribute with
+ - provide concise sources of information
+ - provide _well known_ combination names
+ - or by making tests using [Jest](http://facebook.github.io/jest/docs/en/using-matchers.html)
+ - or improving the build process using [Neutrino](https://neutrino.js.org/)
+ - or making better [flow](https://flow.org/) types
---
+++ test/bets.spec.js
@@ -0,0 +1,20 @@
+describe('Tag bets', () => {
+  describe('Inside bets', () => {
+    test('A - 1 number, Straight up 35:1', () => {})
+    test('B - 2 numbers, Split 17:1', () => {})
+    test('C - 3 numbers, 3 Line 11:1', () => {})
+    test('D - 4 numbers, 4 Corner 8:1', () => {})
+    test('E - 5 numbers, 1st Five 6:1 (double zero roulette table)', () => {})
+    test('E - 4 numbers, 1st Four 8:1 (single zero roulette table - not shown)', () => {})
+    test('F - 6 numbers, 6 Line 5:1', () => {})
+  })
+
+  describe('Outside bets', () => {
+    test('G - 12 numbers, Column 2:1', () => {})
+    test('H - 12 numbers, Dozen 2:1', () => {})
+    test('J - 24 numbers, Split Columns 1:2', () => {})
+    test('K - 24 numbers, Split Dozens 1:2', () => {})
+    test('Even/Odd - Pays even money 1:1', () => {})
+    test('1-18/19-36 (also called Low/High) - Pays even money 1:1', () => {})
+  })
+})
